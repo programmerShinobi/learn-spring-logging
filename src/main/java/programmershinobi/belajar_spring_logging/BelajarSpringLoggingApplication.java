401aa37cd77149911a959630c337d0ec9b423ad4
programmerShinobi <shinobiprogrammer@gmail.com>
2026-02-10 14:46:29 +0700

    [add] first-commit

---
+++ src/main/java/programmershinobi/belajar_spring_logging/BelajarSpringLoggingApplication.java
@@ -0,0 +1,13 @@
+package programmershinobi.belajar_spring_logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmershinobi/belajar_spring_logging/LoggingTest.java
@@ -0,0 +1,18 @@
+package programmershinobi.belajar_spring_logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class LoggingTest {
+
+    @Test
+    void testLog() {
+        log.info("Belajar Java");
+        log.warn("Belajar Spring");
+        log.error("Belajar Programmer Shinobi");
+    }
+
+}
